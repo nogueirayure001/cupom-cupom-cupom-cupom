b5ca5196ab168e828e6d997f1cb7f16256f1bcf3
Yure Nogueira <1993nogueirayure@gmail.com>
2022-07-11 23:22:12 -0300

    feat: added mongoDB connection to the application

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const http = require("http");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
+const MONGOOSE_URL = `mongodb+srv://3xcupommanager:${process.env.MONGODB_PSW}@3xcupom.5b9be.mongodb.net/?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
+
+async function startServer() {
+  await mongoose.connect(MONGOOSE_URL);
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
