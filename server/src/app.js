9b5507aa1268e2416a1710a37d50500cef2c861c
Yure Nogueira <1993nogueirayure@gmail.com>
2022-07-24 20:43:14 -0300

    feat: added scripts to run server, client and deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,23 @@
 import express, { json } from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 import couponsRouter from './routes/coupons.router.js';
 import storesRouter from './routes/stores.router.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 
+app.use(cors());
 app.use(json());
+app.use(express.static(join(__dirname, '..', 'public')));
 
 app.use('/coupons', couponsRouter);
 app.use('/stores', storesRouter);
+app.get('/*', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+});
 
 export default app;
