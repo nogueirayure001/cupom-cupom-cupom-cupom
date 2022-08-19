a8b27313339a86dd6b770a076367362bf6c76d90
Yure Nogueira <1993nogueirayure@gmail.com>
2022-08-18 21:18:07 -0300

    feat: created a class to handle in app caching

---
+++ server/src/utils/cache.utils.js
@@ -0,0 +1,27 @@
+class Cache {
+  constructor() {
+    this.cache = new Map();
+  }
+
+  set(key, value) {
+    const strKey = JSON.stringify(key);
+
+    if (this.cache.has(strKey)) return;
+
+    this.cache.set(strKey, value);
+  }
+
+  get(key) {
+    const strKey = JSON.stringify(key);
+
+    return this.cache.get(strKey);
+  }
+
+  has(key) {
+    const strKey = JSON.stringify(key);
+
+    return this.cache.has(strKey);
+  }
+}
+
+export default Cache;
