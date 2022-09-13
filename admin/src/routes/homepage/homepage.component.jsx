3ca4a7a6ac20315654238f3b7edfe6ddc70530f0
Yure Nogueira <1993nogueirayure@gmail.com>
2022-09-12 22:40:21 -0300

    feat: created iconed buttons for the newsletter and administration page

--- admin/src/routes/homepage/homepage.component.jsx
+++ admin/src/routes/homepage/homepage.component.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../store/auth';
 import { Section } from '../../components/section';
 import { ActionCard } from '../../components/action-card';
 import { ActionsContainer } from './index';
 import { ProtectRoute } from '../../components/protect-route';
 
 function Homepage(props) {
+  const user = useSelector(selectUser);
+
   return (
     <ProtectRoute>
       <Section title='Menu de Ações'>
@@ -19,6 +23,20 @@ function Homepage(props) {
             to='stores'
             image='../../assets/images/store.png'
           />
+
+          <ActionCard
+            name='Newsletter'
+            to='newsletter'
+            image='../../assets/images/newsletter.png'
+          />
+
+          {user.isMaster && (
+            <ActionCard
+              name='Admin'
+              to='admin'
+              image='../../assets/images/admin.png'
+            />
+          )}
         </ActionsContainer>
       </Section>
     </ProtectRoute>
